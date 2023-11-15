ac89d7c6edc0194105c1d519d71457b9499f400e
bishoyrafat <bishoyrafat@yahoo.com>
2023-11-15 12:37:10 +0300

    header added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MainViewComponent } from './main-view/main-view.component';
+import { HeaderComponent } from './header/header.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MainViewComponent,
+    HeaderComponent
   ],
   imports: [
     BrowserModule,
